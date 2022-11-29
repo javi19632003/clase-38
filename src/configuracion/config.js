18303d3081672af402b2021055b51e4ffd2ff495
javi19632003 <javi19632003@gmail.com>
2022-11-29 15:08:22 -0300

    pasaje a Heroku

--- src/configuracion/config.js
+++ src/configuracion/config.js
@@ -1,7 +1,7 @@
 import dotenv              from 'dotenv';
 import path                from 'path';
 dotenv.config({
-    path: path.resolve(process.cwd(), 'produccion.env')
+    path: path.resolve(process.cwd(), process.env.NODE_ENV + '.env')
 });
 
 export default {
